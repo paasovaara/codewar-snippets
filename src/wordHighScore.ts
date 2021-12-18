0b3dc4a608fd20e8cc902590c5dd68ccbabcdf44
Markus Paasovaara <markus.paasovaara@barona.fi>
2021-12-18 10:03:36 +0200

    Cleaned up code

--- src/wordHighScore.ts
+++ src/wordHighScore.ts
@@ -5,25 +5,9 @@ export const wordValue = (str: string): number => {
     .reduce((acc, val) => acc + val, 0);
 }
 
-interface WordValuePair {
-  word: string;
-  value: number;
-}
-
 export const high = (str: string): string => {
   const words: string[] = str.split(" ");
   const values: number[] = words.map(word => wordValue(word));
-  /*const wordsAndValues: WordValuePair[] = words.map(w => {
-    return {
-      word: w,
-      value: wordValue(w)
-    }
-  });
-  
-  console.log(wordsAndValues);
-  return wordsAndValues.indexOf(Math.max());)*/
   const maxIndex = values.indexOf(Math.max(...values));
   return words[maxIndex];
-  //return words[values.indexOf(Math.max)]
-  //return Math.max(...values); 
 }
